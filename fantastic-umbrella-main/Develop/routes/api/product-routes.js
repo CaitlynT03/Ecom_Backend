c1ea5db3e964087cd7eaf35554c1a8009524e204
CaitlynT03 <119358599+CaitlynT03@users.noreply.github.com>
2023-05-29 07:29:26 -0400

    Update product-routes.js

--- fantastic-umbrella-main/Develop/routes/api/product-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/product-routes.js
@@ -7,12 +7,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then(data => {
+    console.log('find all products')
+    res.json(data)});
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+ Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then(data => {
+    console.log('find product by id')
+    res.json(data)
+  });
 });
 
 // create new product
@@ -97,3 +128,4 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
