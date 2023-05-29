98e6fa3a1ca2c4eb03c722368f94bebe4c91ba6d
CaitlynT03 <119358599+CaitlynT03@users.noreply.github.com>
2023-05-29 07:28:38 -0400

    Update category-routes.js

--- fantastic-umbrella-main/Develop/routes/api/category-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/category-routes.js
@@ -6,23 +6,79 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = async Category.findAll({
+      // Include the products associated
+      include: [{
+        model: Product
+      }]
+    });
+    res.json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product
+      }]
+    });
+    res.json(categoryData);
+  } catch (err) {
+    req.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try { 
+    const categoryAdd = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(categoryAdd);
+  } catch (err) {
+    res.status(500).json('{"message": "Error adding category"}');
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryUpdate = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(categoryUpdate);
+  } catch (err) {
+    res.status(500).json('{"message": "Error updating category"}');
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryUpdate = await Category.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(categoryUpdate);
+  } catch (err) {
+    res.status(500).json('{"message": "Error deleting category"}');
+  }
 });
 
 module.exports = router;
